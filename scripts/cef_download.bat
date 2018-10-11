f83271124455031ff44eec50b7907c57e9e4177a
spmn <spmn@users.noreply.github.com>
2018-10-12 00:12:49 +0300

    updated embedded chromium version to chromium 68

--- scripts/cef_download.bat
+++ scripts/cef_download.bat
@@ -6,16 +6,16 @@ mkdir ..\deps\cef_binary
 mkdir cef_download
 cd cef_download
 
-curl -o cef_x86.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_3.3325.1758.g9aea513_windows32_minimal.tar.bz2
+curl -o cef_x86.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_3.3440.1806.g65046b7_windows32_minimal.tar.bz2
 7z x cef_x86.tar.bz2
 7z x cef_x86.tar
-rename cef_binary_3.3325.1758.g9aea513_windows32_minimal Win32
+rename cef_binary_3.3440.1806.g65046b7_windows32_minimal Win32
 move Win32 ..\..\deps\cef_binary\
 
-curl -o cef_x64.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_3.3325.1758.g9aea513_windows64_minimal.tar.bz2
+curl -o cef_x64.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_3.3440.1806.g65046b7_windows64_minimal.tar.bz2
 7z x cef_x64.tar.bz2
 7z x cef_x64.tar
-rename cef_binary_3.3325.1758.g9aea513_windows64_minimal x64
+rename cef_binary_3.3440.1806.g65046b7_windows64_minimal x64
 move x64 ..\..\deps\cef_binary\
 
 cd ..
