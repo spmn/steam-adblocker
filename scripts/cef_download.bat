db0a94d086ea4001be543d9cf16687183f09f341
spmn <spmn@users.noreply.github.com>
2019-01-20 20:32:51 +0200

    cefbuild is a variable now

--- scripts/cef_download.bat
+++ scripts/cef_download.bat
@@ -1,21 +1,23 @@
 @echo off
 
+set CEFBUILD=3.3440.1806.g65046b7
+
 rd /s /q ..\deps\cef_binary
 rd /s /q cef_download
 mkdir ..\deps\cef_binary
 mkdir cef_download
 cd cef_download
 
-curl -o cef_x86.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_3.3440.1806.g65046b7_windows32_minimal.tar.bz2
+curl -o cef_x86.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_%CEFBUILD%_windows32_minimal.tar.bz2
 7z x cef_x86.tar.bz2
 7z x cef_x86.tar
-rename cef_binary_3.3440.1806.g65046b7_windows32_minimal Win32
+rename cef_binary_%CEFBUILD%_windows32_minimal Win32
 move Win32 ..\..\deps\cef_binary\
 
-curl -o cef_x64.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_3.3440.1806.g65046b7_windows64_minimal.tar.bz2
+curl -o cef_x64.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_%CEFBUILD%_windows64_minimal.tar.bz2
 7z x cef_x64.tar.bz2
 7z x cef_x64.tar
-rename cef_binary_3.3440.1806.g65046b7_windows64_minimal x64
+rename cef_binary_%CEFBUILD%_windows64_minimal x64
 move x64 ..\..\deps\cef_binary\
 
 cd ..
