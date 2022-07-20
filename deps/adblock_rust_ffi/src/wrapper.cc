3809f37f9d0960caa84e4dc1d6d068f5b3bc615c
spmn <spmn@users.noreply.github.com>
2022-07-20 19:44:39 +0300

    Add code for the new AdBlock library

---
+++ deps/adblock_rust_ffi/src/wrapper.cc
@@ -0,0 +1,198 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "wrapper.h"  // NOLINT https://github.com/brave/brave-browser/issues/14821
+#include <iostream>
+
+extern "C" {
+#include "lib.h"  // NOLINT
+}
+
+namespace adblock {
+
+bool SetDomainResolver(DomainResolverCallback resolver) {
+  return set_domain_resolver(resolver);
+}
+
+std::vector<FilterList> FilterList::default_list;
+std::vector<FilterList> FilterList::regional_list;
+
+FilterList::FilterList(const std::string& uuid,
+                       const std::string& url,
+                       const std::string& title,
+                       const std::vector<std::string>& langs,
+                       const std::string& support_url,
+                       const std::string& component_id,
+                       const std::string& base64_public_key,
+                       const std::string& desc)
+    : uuid(uuid),
+      url(url),
+      title(title),
+      langs(langs),
+      support_url(support_url),
+      component_id(component_id),
+      base64_public_key(base64_public_key),
+      desc(desc) {}
+
+FilterList::FilterList(const FilterList& other) = default;
+
+FilterList::~FilterList() {}
+
+FilterListMetadata::FilterListMetadata() {}
+
+FilterListMetadata::FilterListMetadata(C_FilterListMetadata* metadata) {
+  char* str_buffer;
+
+  if (filter_list_metadata_homepage(metadata, &str_buffer)) {
+    homepage = absl::make_optional(std::string(str_buffer));
+    c_char_buffer_destroy(str_buffer);
+  }
+
+  if (filter_list_metadata_title(metadata, &str_buffer)) {
+    title = absl::make_optional(std::string(str_buffer));
+    c_char_buffer_destroy(str_buffer);
+  }
+}
+
+FilterListMetadata::~FilterListMetadata() {}
+
+FilterListMetadata::FilterListMetadata(FilterListMetadata&&) = default;
+
+std::pair<FilterListMetadata, std::unique_ptr<Engine>> engineWithMetadata(
+    const std::string& rules) {
+  C_FilterListMetadata* c_metadata;
+  std::unique_ptr<Engine> engine = std::make_unique<Engine>(
+      engine_create_with_metadata(rules.c_str(), &c_metadata));
+  FilterListMetadata metadata = FilterListMetadata(c_metadata);
+  filter_list_metadata_destroy(c_metadata);
+
+  return std::make_pair(std::move(metadata), std::move(engine));
+}
+
+std::pair<FilterListMetadata, std::unique_ptr<Engine>>
+engineFromBufferWithMetadata(const char* data, size_t data_size) {
+  C_FilterListMetadata* c_metadata;
+  std::unique_ptr<Engine> engine = std::make_unique<Engine>(
+      engine_create_from_buffer_with_metadata(data, data_size, &c_metadata));
+  FilterListMetadata metadata = FilterListMetadata(c_metadata);
+  filter_list_metadata_destroy(c_metadata);
+
+  return std::make_pair(std::move(metadata), std::move(engine));
+}
+
+Engine::Engine(C_Engine* c_engine) : raw(c_engine) {}
+
+Engine::Engine() : raw(engine_create("")) {}
+
+Engine::Engine(const std::string& rules) : raw(engine_create(rules.c_str())) {}
+
+Engine::Engine(const char* data, size_t data_size)
+    : raw(engine_create_from_buffer(data, data_size)) {}
+
+void Engine::matches(const std::string& url,
+                     const std::string& host,
+                     const std::string& tab_host,
+                     bool is_third_party,
+                     const std::string& resource_type,
+                     bool* did_match_rule,
+                     bool* did_match_exception,
+                     bool* did_match_important,
+                     std::string* redirect) {
+  char* redirect_char_ptr = nullptr;
+  engine_match(raw, url.c_str(), host.c_str(), tab_host.c_str(), is_third_party,
+               resource_type.c_str(), did_match_rule, did_match_exception,
+               did_match_important, &redirect_char_ptr);
+  if (redirect_char_ptr) {
+    if (redirect) {
+      *redirect = redirect_char_ptr;
+    }
+    c_char_buffer_destroy(redirect_char_ptr);
+  }
+}
+
+std::string Engine::getCspDirectives(const std::string& url,
+                                     const std::string& host,
+                                     const std::string& tab_host,
+                                     bool is_third_party,
+                                     const std::string& resource_type) {
+  char* csp_raw = engine_get_csp_directives(raw, url.c_str(), host.c_str(),
+                                            tab_host.c_str(), is_third_party,
+                                            resource_type.c_str());
+  const std::string csp = std::string(csp_raw);
+
+  c_char_buffer_destroy(csp_raw);
+  return csp;
+}
+
+bool Engine::deserialize(const char* data, size_t data_size) {
+  return engine_deserialize(raw, data, data_size);
+}
+
+void Engine::addTag(const std::string& tag) {
+  engine_add_tag(raw, tag.c_str());
+}
+
+void Engine::removeTag(const std::string& tag) {
+  engine_remove_tag(raw, tag.c_str());
+}
+
+bool Engine::tagExists(const std::string& tag) {
+  return engine_tag_exists(raw, tag.c_str());
+}
+
+void Engine::addResource(const std::string& key,
+                         const std::string& content_type,
+                         const std::string& data) {
+  engine_add_resource(raw, key.c_str(), content_type.c_str(), data.c_str());
+}
+
+void Engine::addResources(const std::string& resources) {
+  engine_add_resources(raw, resources.c_str());
+}
+
+const std::string Engine::urlCosmeticResources(const std::string& url) {
+  char* resources_raw = engine_url_cosmetic_resources(raw, url.c_str());
+  const std::string resources_json = std::string(resources_raw);
+
+  c_char_buffer_destroy(resources_raw);
+  return resources_json;
+}
+
+const std::string Engine::hiddenClassIdSelectors(
+    const std::vector<std::string>& classes,
+    const std::vector<std::string>& ids,
+    const std::vector<std::string>& exceptions) {
+  std::vector<const char*> classes_raw;
+  classes_raw.reserve(classes.size());
+  for (size_t i = 0; i < classes.size(); i++) {
+    classes_raw.push_back(classes[i].c_str());
+  }
+
+  std::vector<const char*> ids_raw;
+  ids_raw.reserve(ids.size());
+  for (size_t i = 0; i < ids.size(); i++) {
+    ids_raw.push_back(ids[i].c_str());
+  }
+
+  std::vector<const char*> exceptions_raw;
+  exceptions_raw.reserve(exceptions.size());
+  for (size_t i = 0; i < exceptions.size(); i++) {
+    exceptions_raw.push_back(exceptions[i].c_str());
+  }
+
+  char* stylesheet_raw = engine_hidden_class_id_selectors(
+      raw, classes_raw.data(), classes.size(), ids_raw.data(), ids.size(),
+      exceptions_raw.data(), exceptions.size());
+  const std::string stylesheet = std::string(stylesheet_raw);
+
+  c_char_buffer_destroy(stylesheet_raw);
+  return stylesheet;
+}
+
+Engine::~Engine() {
+  engine_destroy(raw);
+}
+
+}  // namespace adblock
---
+++ deps/adblock_rust_ffi/src/wrapper.h
@@ -0,0 +1,135 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_ADBLOCK_RUST_FFI_SRC_WRAPPER_H_
+#define BRAVE_COMPONENTS_ADBLOCK_RUST_FFI_SRC_WRAPPER_H_
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/memory/raw_ptr.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+extern "C" {
+#include "lib.h"  // NOLINT
+}
+
+#if defined(ADBLOCK_SHARED_LIBRARY)
+#if defined(WIN32)
+#if defined(ADBLOCK_IMPLEMENTATION)
+#define ADBLOCK_EXPORT __declspec(dllexport)
+#else
+#define ADBLOCK_EXPORT __declspec(dllimport)
+#endif  // defined(ADBLOCK_IMPLEMENTATION)
+#else   // defined(WIN32)
+#if defined(ADBLOCK_IMPLEMENTATION)
+#define ADBLOCK_EXPORT __attribute__((visibility("default")))
+#else
+#define ADBLOCK_EXPORT
+#endif  // defined(ADBLOCK_IMPLEMENTATION)
+#endif
+#else  // defined(ADBLOCK_SHARED_LIBRARY)
+#define ADBLOCK_EXPORT
+#endif
+
+namespace adblock {
+
+typedef C_DomainResolverCallback DomainResolverCallback;
+
+bool ADBLOCK_EXPORT SetDomainResolver(DomainResolverCallback resolver);
+
+class ADBLOCK_EXPORT FilterList {
+ public:
+  FilterList(const std::string& uuid,
+             const std::string& url,
+             const std::string& title,
+             const std::vector<std::string>& langs,
+             const std::string& support_url,
+             const std::string& component_id,
+             const std::string& base64_public_key,
+             const std::string& desc);
+  explicit FilterList(const FilterList& other);
+  ~FilterList();
+
+  const std::string uuid;
+  const std::string url;
+  const std::string title;
+  const std::vector<std::string> langs;
+  const std::string support_url;
+  const std::string component_id;
+  const std::string base64_public_key;
+  const std::string desc;
+
+ private:
+  static std::vector<FilterList> default_list;
+  static std::vector<FilterList> regional_list;
+};
+
+typedef ADBLOCK_EXPORT struct FilterListMetadata {
+  FilterListMetadata();
+  explicit FilterListMetadata(C_FilterListMetadata* metadata);
+  ~FilterListMetadata();
+
+  absl::optional<std::string> homepage;
+  absl::optional<std::string> title;
+
+  FilterListMetadata(FilterListMetadata&&);
+
+  FilterListMetadata(const FilterListMetadata&) = delete;
+} FilterListMetadata;
+
+class ADBLOCK_EXPORT Engine {
+ public:
+  Engine();
+  explicit Engine(C_Engine* c_engine);
+  explicit Engine(const std::string& rules);
+  Engine(const char* data, size_t data_size);
+  void matches(const std::string& url,
+               const std::string& host,
+               const std::string& tab_host,
+               bool is_third_party,
+               const std::string& resource_type,
+               bool* did_match_rule,
+               bool* did_match_exception,
+               bool* did_match_important,
+               std::string* redirect);
+  std::string getCspDirectives(const std::string& url,
+                               const std::string& host,
+                               const std::string& tab_host,
+                               bool is_third_party,
+                               const std::string& resource_type);
+  bool deserialize(const char* data, size_t data_size);
+  void addTag(const std::string& tag);
+  void addResource(const std::string& key,
+                   const std::string& content_type,
+                   const std::string& data);
+  void addResources(const std::string& resources);
+  void removeTag(const std::string& tag);
+  bool tagExists(const std::string& tag);
+  const std::string urlCosmeticResources(const std::string& url);
+  const std::string hiddenClassIdSelectors(
+      const std::vector<std::string>& classes,
+      const std::vector<std::string>& ids,
+      const std::vector<std::string>& exceptions);
+  ~Engine();
+
+  Engine(Engine&&) = default;
+
+ private:
+  Engine(const Engine&) = delete;
+  void operator=(const Engine&) = delete;
+  raw_ptr<C_Engine> raw = nullptr;
+};
+
+std::pair<FilterListMetadata, std::unique_ptr<Engine>> engineWithMetadata(
+    const std::string& rules);
+std::pair<FilterListMetadata, std::unique_ptr<Engine>>
+engineFromBufferWithMetadata(const char* data, size_t data_size);
+
+}  // namespace adblock
+
+#endif  // BRAVE_COMPONENTS_ADBLOCK_RUST_FFI_SRC_WRAPPER_H_
