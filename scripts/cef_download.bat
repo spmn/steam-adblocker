306d54c8ac06175242ab785b768f8f84ac760f1e
spmn <spmn@users.noreply.github.com>
2022-07-24 21:12:35 +0300

    Cut off unnecesary files from CEF binary distribution

--- scripts/cef_download.bat
+++ scripts/cef_download.bat
@@ -7,21 +7,22 @@ set CEFBUILD_ESCAPED=!CEFBUILD:+=%%2B!
 
 rd /s /q ..\deps\cef_binary
 rd /s /q cef_download
-mkdir ..\deps\cef_binary
 mkdir cef_download
 cd cef_download
-
-curl -o cef_x86.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_%CEFBUILD_ESCAPED%_windows32_minimal.tar.bz2
-7z x cef_x86.tar.bz2
-7z x cef_x86.tar
-rename cef_binary_%CEFBUILD%_windows32_minimal Win32
-move Win32 ..\..\deps\cef_binary\
 
 curl -o cef_x64.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_%CEFBUILD_ESCAPED%_windows64_minimal.tar.bz2
 7z x cef_x64.tar.bz2
 7z x cef_x64.tar
-rename cef_binary_%CEFBUILD%_windows64_minimal x64
-move x64 ..\..\deps\cef_binary\
+mkdir cef_binary\Release\x64
+move cef_binary_%CEFBUILD%_windows64_minimal\Release\libcef.lib cef_binary\Release\x64\
+move cef_binary_%CEFBUILD%_windows64_minimal\include cef_binary
 
+curl -o cef_x86.tar.bz2 https://cef-builds.spotifycdn.com/cef_binary_%CEFBUILD_ESCAPED%_windows32_minimal.tar.bz2
+7z x cef_x86.tar.bz2
+7z x cef_x86.tar
+mkdir cef_binary\Release\Win32
+move cef_binary_%CEFBUILD%_windows32_minimal\Release\libcef.lib cef_binary\Release\Win32\
+
+move cef_binary ..\..\deps\
 cd ..
 rd /s /q cef_download
